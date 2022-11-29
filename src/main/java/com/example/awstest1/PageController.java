f4ae8c3e7ca6d82187bf95b1d0093ceeb32d803e
USER <wofov@>
2022-11-29 11:11:16 +0900

    add

--- src/main/java/com/example/awstest1/PageController.java
+++ src/main/java/com/example/awstest1/PageController.java
@@ -9,4 +9,9 @@ public class PageController {
         public String test(){
             return "test";
         }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "test2";
+    }
 }
